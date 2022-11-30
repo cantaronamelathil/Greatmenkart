b34f6ceb2823ae903b5d18b45ae1b074b6671542
cantaronamelathil <111227557+cantaronamelathil@users.noreply.github.com>
2022-11-30 17:49:56 +0530

    final

--- accounts/verify.py
+++ accounts/verify.py
@@ -1,9 +1,10 @@
 import os
 from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
+from decouple import config
 
-client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
-verify = client.verify.services(os.environ['TWILIO_VERIFY_SERVICE_SID'])
+client = Client(config('TWILIO_ACCOUNT_SID'), config('TWILIO_AUTH_TOKEN'))
+verify = client.verify.services(config('TWILIO_VERIFY_SERVICE_SID'))
 
 
 def send(phone):
--- menkart/settings.py
+++ menkart/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
-import greatkart
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3if&p$#kctd_r4nh0fvwf%_i7_v%p4q7m_8c(w43qonx0iim&z'
+# SECRET_KEY = 'django-insecure-3if&p$#kctd_r4nh0fvwf%_i7_v%p4q7m_8c(w43qonx0iim&z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
+
+SECRET_KEY = config('SECRET_KEY')
 ALLOWED_HOSTS = ['*']
+DEBUG = config('DEBUG', default=False,cast=bool)
 
 
 # Application definition
