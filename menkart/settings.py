5ba7a1cdfef950ed353bf1897d5fd657749ec1ce
cantaronamelathil <111227557+cantaronamelathil@users.noreply.github.com>
2022-12-02 14:21:14 +0530

    final2

--- menkart/settings.py
+++ menkart/settings.py
@@ -131,10 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR /'static_root'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),]
+STATICFILES_DIRS = [ BASE_DIR /'static' ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
@@ -142,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
+MEDIA_ROOT = BASE_DIR /'media'
 
 # allow pop-up
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
